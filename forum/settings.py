e1f441bbd1295d12e560641b4c888987f673bcb2
Oved <ovedaydin@gmail.com>
2021-10-26 12:24:03 +0300

    scr

--- forum/settings.py
+++ forum/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hf!#g96fco@po89ctwio-e009ao6mxgiz0%a*5=)3way%=6565'
+SECRET_KEY = '1234'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
